49c8aea90d83a730dffc36c47353a14a3d3751cb
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2023-07-11 20:39:01 +0200

    Update home.js

--- scripts/commands/home.js
+++ scripts/commands/home.js
@@ -8,9 +8,12 @@ world.beforeEvents.chatSend.subscribe(eventData => {
 
         eventData.cancel = true
 
-        let spawnDimension, spawnPosition
-        spawnDimension = player.spawnDimension.id.split(':')[1] ?? 'overworld'
-        spawnPosition = player.getSpawnPosition() ?? new Vector(0, 88, 0)
+        const spawnPoint = player.getSpawnPoint()
+
+        if(!spawnPoint) return player.runCommandAsync(`execute in overworld run tp @s 0 88 0`)
+        
+        const spawnDimension = spawnPoint.dimension.id.split(':')[1]
+        const spawnPosition = new Vector(spawnPoint.x, spawnPoint.y, spawnPoint.z)
         
         player.runCommandAsync(`execute in ${spawnDimension} run tp @s ${spawnPosition.x} ${spawnPosition.y} ${spawnPosition.z}`)
 
