efc2d84341ea0d5656fec651c76de333353ecf57
Fichi 3 <70333084+Fichiiiii@users.noreply.github.com>
2023-09-23 00:39:34 +0200

    Delete scripts directory

--- scripts/scripts/functionality/nonMovableBlocks.js
+++
@@ -1,9 +0,0 @@
-import { world } from '@minecraft/server'
-
-world.beforeEvents.pistonActivate.subscribe(eventData => {
-    const blocks = eventData.piston.getAttachedBlocks()
-
-    blocks.forEach(block => {
-        if (eventData.dimension.getBlock(block).hasTag("nonMovable")) eventData.cancel = true
-    })
-})
